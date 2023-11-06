67cd3694d9d17d83e3de30ff01152bf394872bf6
bam1006 <bam1006@alu.ubu.es>
2023-11-06 09:33:18 +0100

    Nuevo cambio

--- Seminario6_11.R
+++ Seminario6_11.R
@@ -1,12 +1,13 @@
-title: Seminario 
+---
+title: "Seminario Fuentes"
 author:
-  Aitana 
-  Paula 
-  Beatriz 
+  "Aitana Crespo Ferrero" 
+  "Paula Santamaría Velasco" 
+  "Beatriz Alonso Martín"
 date: "`r format(Sys.time(), '%d %B, %Y')`r"
 output:
   html_document:
     df_print: paged
     toc: true
     toc_float: true
-  
+---
