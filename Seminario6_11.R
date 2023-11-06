0268d2156bc5fb411aa8ed396197f3bab6920321
bam1006 <bam1006@alu.ubu.es>
2023-11-06 09:25:35 +0100

    Encabezado YAML

---
+++ Seminario6_11.R
@@ -0,0 +1,12 @@
+title: Seminario 
+author:
+  Aitana 
+  Paula 
+  Beatriz 
+date: "`r format(Sys.time(), '%d %B, %Y')`r"
+output:
+  html_document:
+    df_print: paged
+    toc: true
+    toc_float: true
+  
